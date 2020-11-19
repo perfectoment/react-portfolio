5ac912e091b8211144cbd4e091e29b736cf22e56
Lee Marvin <ryenem8@gmail.com>
2020-11-19 09:39:02 -0800

    fixed images

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import { HashRouter, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Portfolio from './components/pages/Portfolio';
 import Home from "./components/pages/Home"
 import Contact from "./components/pages/Contact"
@@ -11,13 +11,13 @@ class App extends Component{
 
 render() {
   return (
-    <HashRouter basename="/">
+    <Router basename="/">
     <div>
       <Route exact path="/" component={Home} />
-      <Route path="/portfolio" component={Portfolio} />
+      <Route exact path="/portfolio" component={Portfolio} />
       <Route path="/contact" component={Contact} />
     </div>
-  </HashRouter>
+  </Router>
   );
 }
 }
--- portfolio/src/components/Projects/index.js
+++ portfolio/src/components/Projects/index.js
@@ -5,7 +5,7 @@ function Projects(props) {
  return (  
     <div class="card bg-secondary shadow-lg mt-3">
         <div class="card-body bg-secondary">
-        <img class="img img-thumbnail project shadow-lg" src={props.image} alt={`${props.name}`}></img>
+        <img class="img img-thumbnail project shadow-lg" src={process.env.PUBLIC_URL + props.image} alt={`${props.name}`}></img>
         <a class ="font-weight-bold text-light name" href={`${props.link}`}>  {`${props.name}`}</a>
         <br></br>
         <a class ="text-light" href={`${props.github}`}>Link to Github</a>
