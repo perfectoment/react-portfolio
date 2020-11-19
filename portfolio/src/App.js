0ffc91e6989ac019c99cab9640cc3174833daf20
Lee Marvin <ryenem8@gmail.com>
2020-11-19 09:44:11 -0800

    depoly

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import Portfolio from './components/pages/Portfolio';
 import Home from "./components/pages/Home"
 import Contact from "./components/pages/Contact"
@@ -11,13 +11,13 @@ class App extends Component{
 
 render() {
   return (
-    <Router basename="/">
+    <HashRouter basename="/">
     <div>
       <Route exact path="/" component={Home} />
       <Route exact path="/portfolio" component={Portfolio} />
       <Route path="/contact" component={Contact} />
     </div>
-  </Router>
+  </HashRouter>
   );
 }
 }
